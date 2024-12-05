74e09bc5b87c4482de17671b21728b232c9d8b25
zbokleicester <thomasbravo45@gmail.com>
2024-12-05 09:58:19 +0100

    Add files via upload

---
+++ LoupDor.py
@@ -0,0 +1,31 @@
+# JEU 
+
+# définir la grille 
+
+class grille():
+
+    def __init__(self):
+
+        self.Grille = ["."]
+
+        for i in self.Grille:
+
+            while self.Grille < 15:
+
+                print("i")
+
+    def afficher_map(self):
+
+        print('{self.Grille}')
+
+# class Timeout_Tour():
+
+#     def __init__(self):
+
+#         return
+    
+afficher = grille
+afficher.grille()
+
+
+    
